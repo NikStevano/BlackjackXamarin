0efdc032215c8f7a45e4c30234e67211b0b95b54
nik1 <nik@aleniq.com>
2020-07-20 20:34:05 -0400

    Initial Project Commit

---
+++ Blackjack/Blackjack/GamePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Blackjack
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GamePage : ContentPage
+    {
+        private int userBalance;
+        public GamePage(int money)
+        {
+            userBalance = money;
+            InitializeComponent();
+            balance.Text = "Looks like you have $" + userBalance;
+        }
+    }
+}
---
+++ Blackjack/Blackjack/StartPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Blackjack
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class StartPage : ContentPage
+    {
+        private int[] CashLookup = new int[] { 10, 50, 100, 500 };
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+        void OnContinueButtonClicked(object sender, EventArgs e)
+        {
+            if (CashPicker.SelectedIndex >= 0)
+            {
+                int UserCashIndex = CashPicker.SelectedIndex;
+                int UserCash = CashLookup[UserCashIndex];
+                App.StartingCash = UserCash;
+                Application.Current.MainPage = new GamePage(UserCash);
+            }
+            else
+            {
+                DisplayAlert("Error", "Please select a cash amount", "OK");
+                return;
+            }
+        }
+    }
+}
